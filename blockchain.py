d63550620ea018fd11a631a30b9b32fcfcd39a87
semih <semih@uraltelekom.com>
2024-03-25 13:32:35 +0300

    Add transactions, decentralize blockchain and update proof_of_work logic by including timestamp to prevent insufficient nonce range problem.

--- blockchain.py
+++ blockchain.py
@@ -1,48 +1,64 @@
 import datetime
 import hashlib
 import json
+from urllib.parse import urlparse
+import requests
 
 # Create a blockchain class
 
 
 class Blockchain:
     def __init__(self):
-        self._chain = []
-        self._LEADING_ZEROS = '0000'
+        self.__LEADING_ZEROS = '0000'
+        self.__chain = []
+        self.__transactions = []
         # Creates genesis block
-        self.create_block(proof=1, previous_hash='0')
+        self.add_block(proof=1, timestamp=str(
+            datetime.datetime.now()), previous_hash='0')
+        self.__nodes = set()
 
     def get_chain(self):
-        return self._chain
+        return self.__chain
 
-    def create_block(self, proof, previous_hash):
-        block = {'index': len(self._chain) + 1,
-                 'timestamp': str(datetime.datetime.now()),
+    def get_nodes(self):
+        return self.__nodes
+
+    def add_block(self, proof, timestamp, previous_hash):
+        block = {'index': len(self.__chain) + 1,
+                 'timestamp': timestamp,
                  'proof': proof,
-                 'previous_hash': previous_hash
+                 'previous_hash': previous_hash,
+                 'transactions': self.__transactions
                  }
-        self._chain.append(block)
+        self.__transactions = []
+        self.__chain.append(block)
         return block
 
     def get_last_block(self):
-        return self._chain[-1]
+        return self.__chain[-1]
 
     def proof_of_work(self, previous_proof):
+        timestamp = datetime.datetime.now()
+
         new_proof = 1
-        check_proof = False
 
-        while not check_proof:
+        while True:
             hash_operation = hashlib.sha256(
-                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+                str(new_proof**2 - previous_proof**2).encode() + str(timestamp).encode()).hexdigest()
 
-            if(hash_operation[:4] == self._LEADING_ZEROS):
-                check_proof = True
-            else:
-                new_proof += 1
+            if hash_operation[:len(self.__LEADING_ZEROS)] == self.__LEADING_ZEROS:
+                return new_proof, str(timestamp)
 
-        return new_proof
+            new_proof += 1
+
+            # Check if one second has elapsed
+            current_time = datetime.datetime.now()
+            if current_time - timestamp >= datetime.timedelta(seconds=1):
+                timestamp = current_time
+                new_proof = 1
 
     # Hashes block with all properties of block object(index, timestamp, proof, previous_hash etc.)
+
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
@@ -59,13 +75,47 @@ class Blockchain:
 
             previous_proof = previous_block['proof']
             current_proof = current_block['proof']
-            hash_operation = hashlib.sha256(
-                str(current_proof**2 - previous_proof**2).encode()).hexdigest()
+            timestamp = current_block['timestamp']
 
-            if(hash_operation[:4] != self._LEADING_ZEROS):
+            hash_operation = hashlib.sha256(
+                str(current_proof**2 - previous_proof**2).encode() + str(timestamp).encode()).hexdigest()
+
+            if(hash_operation[:len(self.__LEADING_ZEROS)] != self.__LEADING_ZEROS):
                 return False
 
             previous_block = current_block
             current_block_index += 1
 
         return True
+
+    def add_transaction(self, sender, receiver, amount):
+        self.__transactions.append({
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount
+        }
+        )
+
+        last_block = self.get_last_block()
+        return last_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.__nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        longest_chain = None
+        max_length = len(self.__chain)
+
+        for node in self.__nodes:
+            response = requests(f'https://{node}/get_chain')
+            if response.status_code == 200:
+                chain_length = response.json()['chain_length']
+                chain = response.json()['chain']
+                if chain_length > max_length and self.is_chain_valid(chain):
+                    max_length = chain_length
+                    longest_chain = chain
+        if longest_chain:
+            self.__chain = longest_chain
+            return True
+        return False
---
+++ node5002.py
@@ -0,0 +1,111 @@
+from flask import Flask, jsonify, request
+from blockchain import Blockchain
+from uuid import uuid4
+
+
+# Create an api using Flask
+app = Flask(__name__)
+
+# Creating an adress for node
+node_address = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+
+
+@app.route('/mine-block', methods=['GET'])
+def mine_block():
+    last_block = blockchain.get_last_block()
+    last_proof = last_block['proof']
+    mined_proof, timestamp = blockchain.proof_of_work(last_proof)
+    last_hash = blockchain.hash(last_block)
+    blockchain.add_transaction(
+        sender=node_address, receiver='Fatih', amount=200)
+    mined_block = blockchain.add_block(mined_proof, timestamp, last_hash)
+
+    response = {
+        'message': 'Congrats! You\'ve just mined a block!',
+        'index': mined_block['index'],
+        'timestamp': mined_block['timestamp'],
+        'proof': mined_block['proof'],
+        'previous_hash': mined_block['previous_hash'],
+        'transactions':  mined_block['transactions']
+    }
+
+    return jsonify(response), 200
+
+
+@app.route('/get-chain', methods=['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.get_chain(),
+        'chain_length': len(blockchain.get_chain())
+    }
+
+    return jsonify(response), 200
+
+
+@app.route('/replace-chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    response = None
+
+    if is_chain_replaced:
+        response = {
+            'message': 'Chain replaced by the longest chain.',
+            'new_chain': blockchain.get_chain()
+        }
+    else:
+        response = {
+            'message': 'Current chain is already the longest one.',
+            'actual_chain': blockchain.get_chain()
+        }
+
+    return jsonify(response), 200
+
+
+@app.route('/add-transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all(key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(
+        json['sender'], json['receiver'], json['amount'])
+
+    response = {'message': f'This transsaction will be added to block {index}.'}
+    return jsonify(response), 201
+
+
+@app.route('/connect-node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return 'No node', 400
+
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are connected.',
+                'connected_nodes': list(blockchain.get_nodes())
+                }
+    return jsonify(response), 201
+
+
+@app.route('/is-chain-valid', methods=['GET'])
+def is_chain_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.get_chain())
+    validation_message = ''
+
+    if is_valid:
+        validation_message = 'No problem. Blockchain is valid :)'
+    else:
+        validation_message = 'ERROR: Blockchain is invalid :('
+
+    response = {
+        'message': validation_message
+    }
+
+    return jsonify(response), 200
+
+
+# Run the Flask app
+app.run(host='0.0.0.0', port=5002)
