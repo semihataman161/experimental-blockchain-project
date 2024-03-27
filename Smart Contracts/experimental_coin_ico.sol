ca791b457c2894967485e27eac2f15b62f9c2f59
semih <semih@uraltelekom.com>
2024-03-27 11:17:08 +0300

    Add buy and sell experimental coin functionalities.

--- Smart Contracts/experimental_coin_ico.sol
+++ Smart Contracts/experimental_coin_ico.sol
@@ -21,4 +21,18 @@ contract experimental_coin_ico {
     function get_investor_equity_in_experimental_coin(address investor) external view returns(uint) {
         return equity_experimental_coin[investor];
     }
+
+    function buy_experimental_coin(address investor, uint usd_invested) external  
+    can_buy_experimental_coin(usd_invested) {
+        uint experimental_coin_bought = usd_invested * usd_to_experimental_coin;
+        total_experimental_coin_bought += experimental_coin_bought;
+        equity_experimental_coin[investor] += experimental_coin_bought;
+        equity_usd[investor] += usd_invested;
+    }
+
+    function sell_experimental_coin(address investor, uint experimental_coin_sold) external {
+        total_experimental_coin_bought -= experimental_coin_sold;
+        equity_experimental_coin[investor] -= experimental_coin_sold;
+        equity_usd[investor] -= experimental_coin_sold / usd_to_experimental_coin;
+    }
 }
